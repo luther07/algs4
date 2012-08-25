ae2fae0283c01c96e9de8ee92e873ced873f92e8
Mark Johnson <luther07@gmail.com>
2012-08-25 18:03:11 -0500

    only added comments to Percolation

--- bin/Percolation.java
+++ bin/Percolation.java
@@ -1,4 +1,4 @@
-/*-------------------------------------------------------------------------
+/*-----------------------------------------------------------------------------
  * Author:        Mark Johnson
  * Written:       8/22/2012
  * Last Updated:  8/22/2012
@@ -8,19 +8,21 @@
  *
  * Defines a Percolation type, which has a grid, and methods that
  * allow us to measure when the grid percolates.
- * 
- *************************************************************************/
+ *
+ *---------------------------------------------------------------------------*/
 
 public class Percolation {
     private WeightedQuickUnionUF grid; // grid of sites
     private int dimension;             // grid dimensions
     private boolean[] siteStatus;      // open/blocked sites
 
-    /***********************************************************************
+    /**************************************************************************
      * Class constructor method which sets all sites to blocked (false),
      * connects the sites in the virtual bottom to eachother,
-     * and connects the sites in the virtual top to eachother. 
-     ***********************************************************************/
+     * and connects the sites in the virtual top to eachother.
+     *
+     * Updated virtuals, don't need to initialize old virtual top or bottom.
+     *************************************************************************/
     public Percolation(int N) { // create N-by-N grid, with all sites blocked
                                 // What do we do if N equals 1?
         if (N < 1)
@@ -40,16 +42,22 @@ public class Percolation {
         }
 
         // initialize virtual bottom
-        int lastRowFirstColumn = xyTo1D(dimension, 1); // variable simplifies formula
+        int lastRowFirstColumn = xyTo1D(dimension, 1);
         for (int i = 1; i < dimension; i++) {
             grid.union(lastRowFirstColumn, lastRowFirstColumn + i); // ??
         }
     }
 
-    /*********************************************************************
+    /**************************************************************************
      * Method that opens a given site and connects to open neighbors.
-     *********************************************************************/
-    public void open(int i, int j) { // open site (row i, column j)
+     * No changes necessary when making row N+1 into virtual top?
+     *
+     * If you connect a site in row 1, you must connect it to virtual top site.
+     * Virtual top site will be index N.
+     * If you connect a site in row N, you must connect it to virtual bottom
+     * site. Virtual bottom site will be index N*N.
+     *************************************************************************/
+    public void open(int i, int j) {
         if ((i < 1) || (i > dimension) || (j < 1) || (j > dimension))
             throw new java.lang.IndexOutOfBoundsException("index out of bounds");
         int gridIndex = xyTo1D(i, j);
@@ -59,22 +67,24 @@ public class Percolation {
         }
     }
 
-    /**********************************************************************
+    /**************************************************************************
      * Method that says if a given site is open.
-     **********************************************************************/
-    public boolean isOpen(int i, int j) {  // is site (row i, column j) open?
+     * No changes necessary.
+     *************************************************************************/
+    public boolean isOpen(int i, int j) {
         if ((i < 1) || (i > dimension) || (j < 1) || (j > dimension))
-            throw new java.lang.IndexOutOfBoundsException("index out of bounds");
+            throw new java.lang.IndexOutOfBoundsException("invalid index");
         int gridIndex = xyTo1D(i, j);
         return siteStatus[gridIndex];
     }
 
-    /**********************************************************************
+    /**************************************************************************
      * Method that says if a given site is connected to the top.
-     **********************************************************************/
+     * Change to check if gridIndex is connected to new virtual top.
+     *************************************************************************/
     public boolean isFull(int i, int j) {
         if ((i < 1) || (i > dimension) || (j < 1) || (j > dimension))
-            throw new java.lang.IndexOutOfBoundsException("index out of bounds");
+            throw new java.lang.IndexOutOfBoundsException("invalid index");
         int gridIndex = xyTo1D(i, j);
         if (isOpen(i, j)) {
             return grid.connected(0, gridIndex);
@@ -83,9 +93,10 @@ public class Percolation {
         }
     }
 
-    /**********************************************************************
+    /**************************************************************************
      * Method that says whether the system currently percolates.
-     **********************************************************************/
+     * Change to check if new virtual top is connected to new virtual bottom.
+     *************************************************************************/
     public boolean percolates() {
         if (dimension == 1) {
             return isOpen(1, 1);
@@ -94,18 +105,18 @@ public class Percolation {
         }
     }
 
-    /**********************************************************************
+    /**************************************************************************
      * Helper method: converts site location from (row,column) to a 1D index.
-     **********************************************************************/
+     *************************************************************************/
     private int xyTo1D(int i, int j) {
         if ((i < 1) || (i > dimension) || (j < 1) || (j > dimension))
             throw new java.lang.IndexOutOfBoundsException("index out of bounds");
         return dimension*(i - 1) + (j-1);
     }
 
-    /************************************************************************
+    /**************************************************************************
      * Helper method that connects (row i, column j) to all open neighbors.
-     ************************************************************************/
+     *************************************************************************/
     private void connectToNeighbors(int row, int column) {
         if ((row < 1) || (row > dimension) || (column < 1) || (column > dimension))
             throw new java.lang.IndexOutOfBoundsException("index out of bounds");
@@ -125,3 +136,4 @@ public class Percolation {
         }
     }
 }
+
