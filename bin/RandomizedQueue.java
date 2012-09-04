d1514c1b0308ca75b96a8d57f49e1b04f469b6b8
Mark Johnson <luther07@gmail.com>
2012-09-04 07:02:09 -0500

    basic operations implemented, but not correct, project compiles

--- bin/RandomizedQueue.java
+++ bin/RandomizedQueue.java
@@ -12,7 +12,67 @@
 import java.util.Iterator;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
+    private int size;
 
+    /**************************************************************************
+     * Method for default RandomizedQueue construction.
+     *************************************************************************/
+    public RandomizedQueue() {
+        size = 0;
+    }
+
+    /**************************************************************************
+     * Method that says whether the collection is empty.
+     *************************************************************************/
+    public boolean isEmpty() {
+        return true;
+    }
+
+    /**************************************************************************
+     * Method for returning size of the collection.
+     *************************************************************************/
+    public int size() {
+        return size;
+    }
+
+    /**************************************************************************
+     * Method which adds an item to the collection.
+     *************************************************************************/
+    public void enqueue(Item item) {
+        if (item == null) {
+            throw new java.lang.NullPointerException();
+        }
+
+        // TODO : unimplemented
+    }
+
+    /**************************************************************************
+     * Method which returns and removes a random item from the collection.
+     *************************************************************************/
+    public Item dequeue() {
+        if (this.isEmpty()) {
+            throw new java.util.NoSuchElementException();
+        }
+
+        // TODO : unimplemented
+        return null;
+    }
+
+    /**************************************************************************
+     * Method which returns a random item.
+     *************************************************************************/
+    public Item sample() {
+        if (this.isEmpty()) {
+            throw new java.util.NoSuchElementException();
+        }
+
+        // TODO : unimplemented
+        return null;
+    }
+
+    /**************************************************************************
+     * Method for creating an iterator on the collection.
+     *************************************************************************/
     public Iterator<Item> iterator() {
         return new RandomizedQueueIterator();
     }
