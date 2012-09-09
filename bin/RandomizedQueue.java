6258405ba7be83366ed56dc26e1b13e566e04e77
Mark Johnson <luther07@gmail.com>
2012-09-09 11:32:42 -0500

    add test for enqueue and test passes. Added test for dequeue and test fails

--- bin/RandomizedQueue.java
+++ bin/RandomizedQueue.java
@@ -1,4 +1,4 @@
-/*-----------------------------------------------------------------------------
+/*----------------------------------p-------------------------------------------
  * Author:        Mark Johnson
  * Written:       9/4/2012
  * Last Updated:  9/5/2012
@@ -12,24 +12,35 @@
 import java.util.Iterator;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
+    private Item[] randomQueue;
     private int size;
+    private int first;
+    private int last;
 
     /**************************************************************************
      * Method for default RandomizedQueue construction.
+     * Constructs an empty randomized queue
+     * Casts from array of Object to array of Item.
+     * Causes warning when compiling with -Xlint:unchecked.
      *************************************************************************/
     public RandomizedQueue() {
+        randomQueue = (Item[]) new Object[2];
         size = 0;
+        first = 0;
+        last = 0;
     }
 
     /**************************************************************************
      * Method that says whether the collection is empty.
+     * Is the queue empty?
      *************************************************************************/
     public boolean isEmpty() {
-        return true;
+        return size == 0;
     }
 
     /**************************************************************************
      * Method for returning size of the collection.
+     * Return the number of items on the queue.
      *************************************************************************/
     public int size() {
         return size;
@@ -39,6 +50,8 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
      * Method which adds an item to the collection.
      * Complexity: constant amortized time required
      * Must use space proportional to current number of items.
+     * Add the item.
+     * Throw a NullPointerException if the client attempts to add a null item.
      *************************************************************************/
     public void enqueue(Item item) {
         if (item == null) {
@@ -46,6 +59,9 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         }
 
         // TODO : unimplemented
+        randomQueue[last] = item;
+        last++;
+        size++;
     }
 
     /**************************************************************************
@@ -53,6 +69,9 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
      * Item must be chosen uniformly random from all items in the collection.
      * Complexity: constant amortized time required
      * Must use space proportional to current number of items.
+     * Delete and return a random item.
+     * Throw a NoSuchElementExcpetion if the client attempts to dequeue an
+     * item from an empty randomized queue.
      *************************************************************************/
     public Item dequeue() {
         if (this.isEmpty()) {
@@ -60,13 +79,22 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         }
 
         // TODO : unimplemented
+	//        Item removedItem = randomQueue[last-1];
+	//        randomQueue[last-1] = null;
+	//        last--;
+	//        size--;
+	//        return removedItem;
         return null;
+        
     }
 
     /**************************************************************************
      * Method which returns a random item.
      * Complexity: constant amortized time required
      * Must use space proportional to current number of items.
+     * Return (but do not delete) a random item.
+     * Throw a NoSuchElementException if the client attempts to sample an
+     * item from an empty randomized queue.
      *************************************************************************/
     public Item sample() {
         if (this.isEmpty()) {
@@ -79,6 +107,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     /**************************************************************************
      * Method for creating an iterator on the collection.
+     * Return an independent iterator over items in random order
      *************************************************************************/
     public Iterator<Item> iterator() {
         return new RandomizedQueueIterator();
@@ -95,15 +124,25 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
         public RandomizedQueueIterator() {
         }
-
+        /*********************************************************************
+         * Throw an NoSuchElementException if the client calls the next
+         * method in the iterator and there are no more items to return.
+         ********************************************************************/
         public Item next() {
+            if (!this.hasNext()) {
+                throw new java.util.NoSuchElementException();
+            }
+
             return null;
         }
 
         public boolean hasNext() {
             return false;
         }
-
+        /**********************************************************************
+         * Throw an UnsupportedOperationException if the client calls the
+         * remove method in the iterator.
+         *********************************************************************/
         public void remove() {
             throw new java.lang.UnsupportedOperationException();
         }
@@ -147,6 +186,27 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
             testRQueue4.sample();
         } catch (java.util.NoSuchElementException e) {
             StdOut.println("passed");
+        }
+
+        // Test enqueue
+        StdOut.print("Test enqueue method: ");
+        RandomizedQueue<String> testRQueue5 = new RandomizedQueue<String>();
+        testRQueue5.enqueue("Test enqueue method");
+        if (testRQueue5.size() == 1) {
+            StdOut.println("passed");
+        } else {
+            StdOut.println("FAILED");
+        }
+
+        // Test dequeue
+        StdOut.print("Test dequeue method: ");
+        RandomizedQueue<String> testRQueue6 = new RandomizedQueue<String>();
+        testRQueue6.enqueue("Test dequeue method");
+        String testDeque = testRQueue6.dequeue();
+        if (testDeque.equals("Test dequeue method") && testRQueue6.size == 0) {
+            StdOut.println("passed");
+        } else {
+            StdOut.println("FAILED");
         }
     }
 }
