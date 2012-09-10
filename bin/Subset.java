302730763b1c566ca6f7dd04160c6780233d39bf
Mark Johnson <luther07@gmail.com>
2012-09-10 16:21:27 -0500

    just getting started on Subset class

---
+++ bin/Subset.java
@@ -0,0 +1,19 @@
+public class Subset {
+    public static void main(String[] args) {
+        int k; // command line integer
+
+        k = Integer.parseInt(args[0]);
+
+        // use one object of type Deque or RandomizedQueue
+        // use generics properly to avoid casting and compiler warnings
+
+        // reads in a sequence of N strings from standard input,
+        // using StdIn.readString(),
+        // and prints out exactly k of them,
+        // uniformly at random
+
+        // Each item from the sequence can be printed out at most once.
+        // You may assume that k>=0 and no greater than the number
+        // of strings on standard input.
+    }
+}
